8ec59baea93d0e4b32c2ed5b5793eb85edac0c46
Rəvan <110724073+Revan0402@users.noreply.github.com>
2022-09-24 11:50:51 +0400

    Function v2

--- js/legend.js
+++ js/legend.js
@@ -1,7 +1,17 @@
-//Function
+//Function v2
 
-function toplama(){
-    document.write(4+2+'<br>')
+/*function toplama(a,b){
+    document.write(a+b)
 }
-toplama()
-toplama()
+toplama(4,2)*/
+
+/*function toplama(a,b=3){
+    document.write(a+b)
+}
+var cem=toplama(4)*/
+
+/*function toplama(a,b=0){
+    return a+b
+}
+
+document.write(toplama(40,5))*/
